55b1c19c7e0e38633b89dac986bcb626fbff9e69
Almudena De La Rosa <almudenaxico@gmail.com>
2021-08-25 18:56:10 +0000

    fisrt commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { func } from "prop-types";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [lists, setLists] = useState(["Task", "Task2"]);
+	const [newTask, setNewTask] = useState("");
+
+	function changeValue(event) {
+		setNewTask(event.target.value);
+	}
+
+	function pressEnter(event) {
+		if (event.keyCode === 13) {
+			let repeated = lists.findIndex(list => list === newTask);
+			if (repeated === -1) {
+				setLists([...lists, newTask]);
+				setNewTask("");
+			}
+		}
+	}
+
+	function deleteTask(indexToRemove) {
+		setLists(lists.filter((list, index) => index !== indexToRemove));
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid bg-light">
+			<h1 className="text-center">TODOS</h1>
+			<input
+				value={newTask}
+				className="p-2 w-100 bg-transparent border-0"
+				placeholder="What needs to be done?"
+				id="newTask"
+				onChange={changeValue}
+				onKeyDown={pressEnter}
+			/>
+			<ul className="lists">
+				{lists.map((list, index) => (
+					<li className="list" key={index}>
+						<span>{list}</span>
+						<i
+							onClick={() => {
+								deleteTask(index);
+							}}
+							className="fas fa-trash-alt delete"></i>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
